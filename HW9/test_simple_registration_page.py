590ab3ed59294db8fc97300663989efc66ff1177
Vladimir Krinitsyn <Premio1896@rambler.ru>
2024-10-17 01:57:02 +0300

    hw_10 fix 2

--- HW9/test_simple_registration_page.py
+++ HW9/test_simple_registration_page.py
@@ -2,7 +2,7 @@ from HW3.data.users import user_semen
 from HW9.pages.simple_registration_page import SimpleRegistrationPage
 
 
-def test__simple_registration():
+def test_simple_registration():
     registration_page = SimpleRegistrationPage()
     registration_page.open()
     registration_page.register(user_semen)

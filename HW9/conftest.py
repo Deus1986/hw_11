c4a4e1892bf17386d0fa77e85bb5f41f75388878
Vladimir Krinitsyn <Premio1896@rambler.ru>
2024-10-17 02:14:12 +0300

    hw_10 fix 6

--- HW9/conftest.py
+++ HW9/conftest.py
@@ -6,8 +6,9 @@ from selenium.webdriver.chrome.options import Options
 from HW9.utils import attach
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="function", autouse=True)
 def setup_browser(request):
+    browser.config.base_url = "https://demoqa.com/automation-practice-form/"
     options = Options()
     capabilities = {
         "browserName": "chrome",
@@ -19,6 +20,7 @@ def setup_browser(request):
     }
 
     options.capabilities.update(capabilities)
+    options.page_load_strategy = "eager"
     driver = webdriver.Remote(
         command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options)
@@ -31,18 +33,4 @@ def setup_browser(request):
     attach.add_html(browser)
     attach.add_logs(browser)
     attach.add_video(browser)
-    browser.quit()
-
-
-@pytest.fixture(scope='function', autouse=True)
-def browser_management():
-    browser.config.base_url = "https://demoqa.com/automation-practice-form/"
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option("excludeSwitches", ['enable-automation'])
-    options.page_load_strategy = "eager"
-    options.add_argument('--headless')
-    browser.config.driver_options = options
-    # browser.config.driver.maximize_window()
-
-    yield
     browser.quit()
